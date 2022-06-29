7fd69f8c0d5987cf916145d62fd97130f00d58bd
Vasiliy Tyubek <v.tyubek@corp.mail.ru>
2022-06-29 10:50:26 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,5 +4,9 @@ terraform {
       source  = "vk-cs/vkcs"
       version = "0.1.6"
     }
+    openstack = {
+      source = "terraform-provider-openstack/openstack"
+      version = "1.47.0"
+    }
   }
 }
